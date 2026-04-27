4341400f5d8d7b288e552a09778b3e07b1826583
ciguarin <ian@ianguarin.com>
2026-04-27 17:21:23 -0400

    Implement get_windows() function

---
+++ src/windows.py
@@ -0,0 +1,24 @@
+from AppKit import NSWorkspace
+from ApplicationServices import AXUIElementCreateApplication, AXUIElementCopyAttributeValue
+from src.types import Window
+
+
+def get_windows() -> list[Window]:
+    running_apps = [app for app in NSWorkspace.sharedWorkspace().runningApplications() if app.activationPolicy() == 0]
+
+    windows = []
+
+    for app in running_apps:
+        pid = app.processIdentifier()
+        app_ax_obj = AXUIElementCreateApplication(pid)
+
+        err, window = AXUIElementCopyAttributeValue(app_ax_obj, "AXWindows", None)
+
+        if err == 0 and window:
+            for window_ax_obj in window:
+                err, title = AXUIElementCopyAttributeValue(window_ax_obj, "AXTitle", None)
+
+                if err == 0 and title:
+                    windows.append(Window(title, app.localizedName(), pid, window_ax_obj))
+
+    return windows
